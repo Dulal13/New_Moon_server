4010a1115525dd2b52922c26a9560ae26a73aa79
Dulal <dulalmiah70113@gmail.com>
2022-02-18 06:41:31 +0600

    Registration done

---
+++ dashboard.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- favicon -->
+    <link rel="shortcut icon" href="images/new_moon.png" type="image/x-icon">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- css -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>New Moon</title>
+</head>
+
+<body>
+    <div class="container">
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-warning text-dark mt-2">
+                <a class="navbar-brand" href="#">
+                    <img src="images/logo.png" alt="" id="logo">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#about">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#team">Team</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="course.html">Course</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#job">Apply</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+
+    <div class="container mt-5 mb-5 banner" id="about">
+        <div class="row">
+            <div class="col-md-6 col-sm-12 txt">
+                <h2>Welcome to <span style="color:tomato">New Moon</span></h2>
+
+                <small>
+                    <p> We're dedicated to providing you the very best of <span
+                            style="color:tomato; font-weight: bold;">Website , Mobile app, IOS</span> app and more.
+                        Founded
+                        in 2022 by Md. Dulal Miah, new moon has come a long way from its beginnings in Gazipur.</p>
+
+                    <p>When <span style="color:tomato; font-weight: bold;">Dulal</span> first started out, his passion
+                        for
+                        android app drove them to start their own business.
+                        We hope you enjoy our products as much as we enjoy offering them to you. If you have any
+                        questions
+                        or comments, please don't hesitate to contact us.</p>
+                </small>
+            </div>
+            <div class="col-md-6 col-sm-12">
+                <img src="images/banner/working-girl.png" class="img-fluid mx-auto d-block aboutImage" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div class="container" id="team">
+        <h3 style="text-align: center;">Members</h3>
+    </div>
+
+
+    <div class="container carousel mt-5 mb-5">
+        <div id="carouselExampleControls" class="carousel slide bg" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="row align-items-center">
+                        <div class="col-md-7">
+                            <h4>UI/UX Designer</h4>
+                            <p>A UI Designer — or User Interface Designer — designs all the screens through which a user
+                                will move while using a website, app, or video game.</p>
+                        </div>
+                        <div class="col-md-5">
+                            <img src="images/team_member/team-01.png" class="d-block w-100 carousel-img" alt="...">
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="row  align-items-center">
+                        <div class="col-md-7">
+                            <h4>Web Developer</h4>
+                            <p>A Web Developer is responsible for the coding, design and layout of a website according
+                                to a company's specifications.
+                            </p>
+                        </div>
+                        <div class="col-md-5">
+                            <img src="images/team_member/team-02.png" class="d-block w-100 carousel-img" alt="...">
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="row align-items-center">
+                        <div class="col-md-7">
+                            <h4>Android App Developer</h4>
+                            <p>Monitoring the performance of live apps and work on optimizing them at the code level.
+                                Identifying and resolving bottlenecks, rectifying bugs and enhancing application
+                                performance.</p>
+                        </div>
+                        <div class="col-md-5">
+                            <img src="images/team_member/team-03.png" class="d-block w-100 carousel-img" alt="...">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+
+
+    <div class="container mt-5 mb-5" id="job">
+        <div class="row">
+            <div class="col-md-6 col-sm-12" style="padding: 50px;">
+                <h2>Find Your New Dream Job with <span style="color:tomato; font-weight: bold;">New Moon</span></h2>
+                <button class="btn btn-success mt-3"><a href="job.html">Apply Now</a></button>
+            </div>
+
+            <div class="col-md-6 col-sm-12">
+                <img src="images/Interview/interview.png" class="img-fluid mx-auto d-block aboutImage" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div class="container address mt-5 mb-5">
+
+        <div class="row address">
+            <div class="col-md-4">
+
+                <ul>
+                    <li><small>About New Moon</small></li>
+                    <li><small>Mobile App</small></li>
+                    <li><small>Website</small></li>
+                    <li><small>Website</small></li>
+                </ul>
+
+            </div>
+            <div class="col-md-2">
+                <ul>
+                    <li><small>SUPPORT</small></li>
+                    <li><small>Contact Us</small></li>
+                </ul>
+            </div>
+            <div class="col-md-6">
+                <h5>DOWNLOAD THE NEW MOON APP in APP STORE and PLAY STORE</h5>
+                <small>Take New Moon with you and learn anywhere, anytime on your phone and tablet.</small>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- js -->
+    <script src=" https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ registration.php
@@ -0,0 +1,181 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Registration</title>
+    <style>
+        *{
+            padding:0;
+            margin:0;
+            box-sizing: border-box;
+        }
+        body{
+            background: rgb(219,226,226);
+        }
+        .row{
+            background: white;
+            border-radius: 30px;
+            box-shadow: 12px 12px 22px grey;
+        }
+        img{
+            border-top-left-radius: 30px;
+            border-bottom-left-radius: 30px;
+            border-top-right-radius: 30px;
+        }
+        .btn{
+            color:black;
+            background:white;
+            border:2px solid lightslategray;
+            font-weight:bold;
+        }
+        .btn:hover{
+
+            color:white;
+            background: black;
+            font-weight:bold;
+        }
+    </style>
+  </head>
+  <body>
+
+  <?php
+    session_start();
+    require('dp.php');
+    
+
+    if (isset($_REQUEST['username'])) {
+        
+        $username = stripslashes($_REQUEST['username']);
+        $username = mysqli_real_escape_string($con, $username);
+
+        $email    = stripslashes($_REQUEST['email']);
+        $email    = mysqli_real_escape_string($con, $email);
+
+        $password = stripslashes($_REQUEST['password']);
+        $password = mysqli_real_escape_string($con, $password);
+
+        $cpassword = stripslashes($_REQUEST['cpassword']);
+        $cpassword = mysqli_real_escape_string($con, $cpassword);
+
+        $create_datetime = date("Y-m-d H:i:s");
+
+       
+    //     if( $password ==  $cpassword){
+    //     $query    = "INSERT into users(username, password, email, create_datetime)
+    //     VALUES ('$username', '" . md5($password) . "', '$email', '$create_datetime')";
+    //     $result   = mysqli_query($con, $query); 
+    //     if ($result) {
+    //         header("Location: login.php");
+    //     }
+    //     } else {
+    //         // echo '<script>alert("password not match")</script>';
+    //         // header("Location: registration.php");
+    //         echo ("<script LANGUAGE='JavaScript'>
+    // window.alert('Password Not Match');
+    // window.location.href='registration.php';
+    // </script>");
+        // }
+
+        if( $password ==  $cpassword){
+            $select = mysqli_query($con, "SELECT `email` FROM `users` WHERE `email` = '".$_POST['email']."'") or exit(mysqli_error($con));
+            if(mysqli_num_rows($select)) {
+
+                echo ("<script LANGUAGE='JavaScript'>
+                  window.alert('Email already used');
+                  window.location.href='registration.php';
+                  </script>");
+                   
+               }
+            else{
+                $query    = "INSERT into users(username, password, email, create_datetime)
+                 VALUES ('$username', '" . md5($password) . "', '$email', '$create_datetime')";
+                 $result   = mysqli_query($con, $query); 
+                 if ($result) {
+                    header("Location: login.php");
+                 }
+            }
+        }
+        else{
+            echo ("<script LANGUAGE='JavaScript'>
+            window.alert('Password not match');
+            window.location.href='registration.php';
+            </script>");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    } 
+    else {
+?>
+    
+    <section class="form my-5">
+        <div class="container">
+            <div class="row no-gutters">
+                <div class="col-lg-5">
+                    <img src="LRO_image/registration.gif" class="img-fluid" alt="">
+                </div>
+                <div class="col-lg-7 px-5 pt-5">
+
+                    <h1 class="font-weight-bold py-3">Registration</h1>
+                    <h4>Sign Up For Your Account</h4>
+                    <form  action="" method="post">
+                        <div class="form-row">
+                            <div class="col-lg-7">
+                                <input type="text" name="username" id="username" class="form-control my-3 p-2" placeholder="User Name"Required>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="col-lg-7">
+                                <input type="email" name="email" id="email" class="form-control my-3 p-2" placeholder="Email-Adress"Required>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="col-lg-7">
+                                <input type="password" name="password" id="password" class="form-control my-3 p-2" placeholder="Password"Required>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="col-lg-7">
+                                <input type="password" name="cpassword" id="cpassword" class="form-control my-3 p-2" placeholder="Confirm Password"Required>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="col-lg-7">
+                                <input type="submit" name="submit" id="submit"submit class="form-control my-3 p-2 btn" value="Submit"Required>
+                            </div>
+                        </div>
+                        <p> Have you an account? <a href="login.php">Login</a></p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php
+    }
+?>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
